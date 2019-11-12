508bcb424a5764dbc70077e341a0b3d8128f0062
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-11-12 19:11:54 -0300

    Update get_bam.sh

--- get_bam.sh
+++ get_bam.sh
@@ -3,23 +3,20 @@
 #inctime=/dados/dmdpesq/BAM_grib2/bin/inctime
 inctime=/dados/dmdpesq/Proj_GFS/bin/inctime/inctime
 
-
-
 getGrib() {
 
   #inctime analise
   fct=24
-  
+
   #inctime dataprev
   tfct=${1}
-  
-  rm /dados/dmdpesq/BAM_grib2/${yyyymm}/${yyyymmdd_anl}/${tfct}/*
+ 
 
   for hhi in $(seq -w 0 12 12)
   do
-    datai=20191107${hhi}
-    dataf=20191111${hhi}
-
+    datai=${2}${hhi}
+    dataf=${3}${hhi}
+    #echo "${datai}"
 
 
     data=${datai}
@@ -30,26 +27,30 @@ getGrib() {
       yyyymm=$(echo ${data} | cut -c 1-6)
       yyyymmdd_anl=$(echo ${data} | cut -c 1-8)
 
-
-      echo ${data}
+      #echo ${data}
       dataanl=${data}
       for hh in $(seq -w 0 6 18)
       do
         dataprev=$(${inctime} ${dataanl} +${tfct}hr %y4%m2%d2%h2)
         yyyymmdd_prev=$(echo ${dataprev} | cut -c 1-8)
-        echo "$dataprev"
+        echo "$hh"
+        echo "analise $dataanl"
+        echo "previsao $dataprev"
+        
 
-        wget -c http://ftp.cptec.inpe.br/modelos/io/tempo/global/BAM/${dataanl}/BAM${dataanl}${yyyymmdd_prev}${hh}.grib2
+        ###wget -c http://ftp.cptec.inpe.br/modelos/io/tempo/global/BAM/${dataanl}/BAM${dataanl}${yyyymmdd_prev}${hh}.grib2
+        ###wget -c http://ftp.cptec.inpe.br/modelos/io/tempo/global/BAM/${dataanl}/GPOSNMC{dataanl}${yyyymmdd_prev}${hh}P.grib2	
+        ###wget -c http://ftp.cptec.inpe.br/modelos/io/tempo/global/BAM/${dataanl}/GPOSREG{dataanl}${yyyymmdd_prev}${hh}.grib2
         #sleep 4s
         #echo "${yyyymm}"
         #echo "${yyyymm}/${yyyymmdd}"
         #echo "movendo arquivo BAM${data}${yyyymmdd}${hh}.grib2 para /dados/dmdpesq/BAM_grib2/${yyyymm}/${yyyymmdd_anl} "
         
       done
-      echo "${yyyymmdd_anl}/${tfct}"
-      mkdir /dados/dmdpesq/BAM_grib2/${yyyymm}/${yyyymmdd_anl}
-      mkdir /dados/dmdpesq/BAM_grib2/${yyyymm}/${yyyymmdd_anl}/${tfct}
-      mv *.grib2 /dados/dmdpesq/BAM_grib2/${yyyymm}/${yyyymmdd_anl}/${tfct}
+      #echo "${yyyymmdd_anl}/${tfct}"
+      #mkdir /dados/dmdpesq/BAM_grib2/${yyyymm}/${yyyymmdd_anl}${hhi}
+      #mkdir /dados/dmdpesq/BAM_grib2/${yyyymm}/${yyyymmdd_anl}${hhi}/${tfct}
+      #mv *.grib2 /dados/dmdpesq/BAM_grib2/${yyyymm}/${yyyymmdd_anl}${hhi}/${tfct}
 
       data=$(${inctime} ${data} +${fct}hr %y4%m2%d2%h2)
 
@@ -57,12 +58,16 @@ getGrib() {
   done
 }
 
+date=$(date +'%Y%m%d')
+datef=$(date -d ' +1 days' '+%Y%m%d')
+echo "$date"
+echo "$datef"
 for previsao in $(seq 24 24 168)
 do 
     echo "*****************"
     echo "$previsao"
     echo "*****************"
-    getGrib ${previsao}
+    getGrib ${previsao} ${date} ${datef}
 done
 
 exit 0
